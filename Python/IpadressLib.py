eeab29b3f27fdd369b908ecd9ded06a747cd5152
Alessandro Prandi <alessandro.prandi@itiscuneo.eu>
2023-11-30 18:26:54 +0100

    mod Es

--- Python/IpadressLib.py
+++ Python/IpadressLib.py
@@ -1,16 +1,30 @@
 import ipaddress
 
+
 def main():
-    ipv4 = input("inserisici un IPv4 di rete: ")
-    subnet = input("inserisci una subnet senza CDR: ")
-    
-    if ipaddress.ip_address(ipv4).is_private == True:
+    ipv4 = input("inserisici un IPv4: ")
+    subnet = input("inserisci una subnet in CDR(es. /24): ")
+    ipv4pieno = ipv4 + subnet
+    ip = ipaddress.ip_address(ipv4)
+
+    if ip.is_private == True:
         print(f"L'IP è privato")
-    
-    if ipaddress.ip_address(ipv4).is_loopback == True:
+    else:
+        print(f"L'IP è pubblico")
+
+    if ip.is_loopback == True:
         print(f"L'IP è di loopback")
-        
-    print(list(ipaddress.ip_network(ipv4).hosts()))
-        
-if __name__ == '__main__':
+
+    iprete = ipaddress.IPv4Network(ipv4pieno, strict=False)
+    if ipv4pieno == str(iprete):
+        print(f"L'Ip è di rete")
+    else:
+        print(f"L'Ip di rete è {iprete}")
+
+    host = list(iprete.hosts())
+    print(f"Il primo Ip utilizzabile è {host[0]} {subnet}")
+    print(f"L'ultimo Ip utilizzabile è {host[-1]} {subnet}")
+
+
+if __name__ == "__main__":
     main()
